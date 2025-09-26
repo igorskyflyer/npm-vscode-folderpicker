a7759cb6c731b49eccbf668854d48055ee3c026b
igorskyflyer <igor.dvlpr@gmail.com>
2025-09-26 18:35:05 +0200

    Add ActionType.ts

---
+++ src/types/ActionType.ts
@@ -0,0 +1,12 @@
+// Author: Igor Dimitrijević (@igorskyflyer)
+
+export enum ActionType {
+  Unspecified = -2,
+  Close = -1,
+  Navigate = 0,
+  CreateFolder = 1,
+  ChildFolder = 2,
+  ParentFolder = 3,
+  PickFolder = 4,
+  Clear = 5
+}
