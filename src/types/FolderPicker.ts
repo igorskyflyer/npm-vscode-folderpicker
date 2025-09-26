328f05298fcf4e2adcc1e310a7902a0374a19413
igorskyflyer <igor.dvlpr@gmail.com>
2025-09-26 18:35:16 +0200

    Add FolderPicker.ts

---
+++ src/types/FolderPicker.ts
@@ -0,0 +1,6 @@
+// Author: Igor Dimitrijević (@igorskyflyer)
+
+import type { QuickPick } from 'vscode'
+import type { IFolderQuickPickItem } from '../interfaces/IFolderQuickPickItem.js'
+
+export type FolderPicker = QuickPick<IFolderQuickPickItem>
