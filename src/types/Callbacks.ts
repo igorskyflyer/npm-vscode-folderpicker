e451f2c72da475049103b78a6f469effb7412500
igorskyflyer <igor.dvlpr@gmail.com>
2025-09-26 21:39:12 +0200

    Refactor callbacks

--- src/types/Callbacks.ts
+++ src/types/Callbacks.ts
@@ -2,9 +2,9 @@
 
 import type { QuickPick, QuickPickItem } from 'vscode'
 
-export type UnspecifiedActionCallback<T extends QuickPickItem = QuickPickItem> =
+export type UnknownActionCallback<T extends QuickPickItem = QuickPickItem> =
   (ui: QuickPick<T>) => void
 export type ErrorCallback = (error: Error) => void
 export type FolderActionCallback = (folderPath: string) => void
-export type ActionCallback = () => void
-export type FetchCallback = ActionCallback
+export type UICallback = () => void
+export type FetchCallback = () => void
