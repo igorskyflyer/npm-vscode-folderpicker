7f6a1e9587dcec6d5553e4a2dbd4ec4a150b5d8a
igorskyflyer <igor.dvlpr@gmail.com>
2025-09-26 18:33:52 +0200

    Add Icon.ts

---
+++ src/Icon.ts
@@ -0,0 +1,9 @@
+// Author: Igor Dimitrijević (@igorskyflyer)
+
+export const ICON_FOLDER: string = '$(folder)'
+export const ICON_FOLDER_UP: string = '$(folder-opened)'
+
+export const ICON_CREATE: string = '$(new-folder)'
+export const ICON_NAVIGATE: string = '$(file-submodule)'
+export const ICON_PICK: string = '$(folder-active)'
+export const ICON_CLEAR: string = '$(close)'
