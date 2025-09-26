9e44fc3fba165669598ef8d3d930f4d9031c3c2a
igorskyflyer <igor.dvlpr@gmail.com>
2025-09-26 19:06:11 +0200

    Merge callback types

--- src/types/ActionCallback.ts
+++
@@ -1,8 +0,0 @@
-// Author: Igor Dimitrijević (@igorskyflyer)
-
-import type { QuickPick, QuickPickItem } from 'vscode'
-
-export type ActionCallback<T extends QuickPickItem = QuickPickItem> = (
-  folderPath: string,
-  ui: QuickPick<T>
-) => void
