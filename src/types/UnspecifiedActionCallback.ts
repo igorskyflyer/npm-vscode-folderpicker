b1bc59650d868333fc2c6f6f1d42e872db6948a9
igorskyflyer <igor.dvlpr@gmail.com>
2025-09-26 19:06:31 +0200

    Merge callback types

--- src/types/UnspecifiedActionCallback.ts
+++
@@ -1,6 +0,0 @@
-// Author: Igor Dimitrijević (@igorskyflyer)
-
-import type { QuickPick, QuickPickItem } from 'vscode'
-
-export type UnspecifiedActionCallback<T extends QuickPickItem = QuickPickItem> =
-  (ui: QuickPick<T>) => void
